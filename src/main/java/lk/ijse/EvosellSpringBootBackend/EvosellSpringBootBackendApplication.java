49eb802b8610ea070ab55582ccc91852c9aef101
Kavindu <gayanthakavindu3@gmail.com>
2024-10-19 01:55:13 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/EvosellSpringBootBackend/EvosellSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.EvosellSpringBootBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvosellSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvosellSpringBootBackendApplication.class, args);
+	}
+
+}
